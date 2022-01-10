abdc21ec014682c4bac1a21e6c2f9c441f9a04c2
Dono <Dono@gmail.com>
2022-01-10 16:05:59 +0000

    update dev add

--- data/Commands/Dev only/dev add.js
+++ data/Commands/Dev only/dev add.js
@@ -1,7 +1,7 @@
 module.exports = {
  	name: "deva",
     code: `
-    $setGlobalUserVar[Cash;$sum[$getGlobalUserVar[Cash];$random[1.49999999999998e+26;1.49999999999998e+27]]]
-    I have add to you account $random[1.49999999999999+26;1.49999999999999e+27] 
+    $setGlobalUserVar[Cash;$sum[$getGlobalUserVar[Cash];$random[1.49999999999999e+27;1.49999999999999e+28]]
+    I have add to you account $random[1.4999999999999e+27;1.49999999999999e+28] 
     $onlyForIDs[745691705390399590;This can be used only by the developer only]`
 }
