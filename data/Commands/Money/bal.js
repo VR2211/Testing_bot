036a8f6596398ec51d37514f8bdacf8874e42560
Vr_Laptop <Vr_laptop>
2022-02-13 16:48:03 +0200

    Fix index.js to connect to db

--- data/Commands/Money/bal.js
+++ data/Commands/Money/bal.js
@@ -5,8 +5,8 @@ code:`$addTimestamp[1]
 $author[1;$username[$mentioned[1;yes]] Bal]
 $color[1;29af57]
 $addField[1;:money_with_wings: Net Worth:;$$numberSeparator[$sum[$getGlobalUserVar[Cash;$authorID;economy;$mentioned[1;yes]];$getGlobalUserVar[Bank;$authorID;economy;$mentioned[1;yes]]]]]
-$addField[1; :moneybag: Bank;$$numberSeparator[$getGlobalUserVar[Bank;$authorID;economy;$mentioned[1;yes]]]]
-$addField[1;:dollar: Cash;$$numberSeparator[$getGlobalUserVar[Cash;$authorID;economy;$mentioned[1;yes]]]]
+$addField[1; :moneybag: Bank;$$numberSeparator[$getGlobalUserVar[Bank;$authorID;economy;$mentioned[1;yes];$authorID;economy]]]
+$addField[1;:dollar: Cash;$$numberSeparator[$getGlobalUserVar[Cash;$authorID;economy;$mentioned[1;yes];$authorID;economy]]]
 $onlyIf[$isBot[$mentioned[1;yes]]!=true;Bots don't have money!]
 $onlyIf[$getGlobalUserVar[acc;$authorID;economy]==true;You need to make an acc You can make an acc by doing  $getServerVar[prefix;$clientID;prefix]begin]`
 })
