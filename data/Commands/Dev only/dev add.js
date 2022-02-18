329f95219938427d1724e31537ef30ada456bf05
VR <71366742+gamelife2572@users.noreply.github.com>
2022-02-18 16:04:53 +0200

    update dev add.js

--- data/Commands/Dev only/dev add.js
+++ data/Commands/Dev only/dev add.js
@@ -1,7 +1,7 @@
 module.exports = {
  	name: "deva",
     code: `
-    $setGlobalUserVar[Cash;$sum[$getGlobalUserVar[Cash;$authorID;economy ];$random[1.49999999999998e+26;1.49999999999998e+27]];$authorID;economy]
+    $setGlobalUserVar[Cash;$sum[$getGlobalUserVar[Cash;$authorID;economy];$random[1.49999999999998e+26;1.49999999999998e+27]];$authorID;economy]
     I have add to you account $random[1.49999999999998e+26;1.49999999999998e+27] 
     $onlyForIDs[745691705390399590;822525798065373265;This can be used only by the developer only]`
 }
